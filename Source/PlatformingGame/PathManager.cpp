485c608927d4aec2ef34f087735541509bf0766f
charlie-flynn <charlie.flynn.business@gmail.com>
2025-09-12 15:28:11 -0500

    Initial commit

---
+++ Source/PlatformingGame/PathManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PathManager.h"
+
+// Sets default values
+APathManager::APathManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APathManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APathManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
